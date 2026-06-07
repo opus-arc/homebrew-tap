76a86d705892af3485432293a6008a96b8d4b3b4
Ziyang Tan <arcopus07@gmail.com>
2026-06-07 15:26:34 +0800

    Update download URL for bBpiano Lite

--- Formula/bbpiano-lite.rb
+++ Formula/bbpiano-lite.rb
@@ -1,7 +1,7 @@
 class BbpianoLite < Formula
   desc "Command-line acoustic engine laboratory for bBpiano Lite"
   homepage "https://github.com/opus-arc/bBpiano"
-  url "https://github.com/opus-arc/bBpiano/releases/download/v0.1.0-L/bBpiano-Lite-v0.1.0-L-macOS-arm64.zip"
+  url "https://github.com/opus-arc/bBpiano/releases/download/v0.1.0-L1/bBpiano-Lite-v0.1.0-L-macOS-arm64.zip"
   sha256 "0c2c2b8ef1d70c5aead5dedca30995217536e894c34300b09c5bff94947eb107"
   license "PolyForm-Internal-Use-1.0.0"
 
