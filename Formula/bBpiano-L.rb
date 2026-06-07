b6d4a84e6fa1e7adf194029baa301b7940932eda
Ziyang Tan <arcopus07@gmail.com>
2026-06-07 17:43:56 +0800

    Update sha256 checksum for bBpiano-L formula

--- Formula/bBpiano-L.rb
+++ Formula/bBpiano-L.rb
@@ -2,7 +2,7 @@ class BbpianoL < Formula
   desc "Command-line acoustic engine laboratory for bBpiano Lite"
   homepage "https://github.com/opus-arc/bBpiano"
   url "https://github.com/opus-arc/bBpiano/releases/download/v0.1.0-L/bBpiano-Lite-v0.1.0-L-macOS-arm64.zip"
-  sha256 "90c080c1fd705a5b0fac29829f5cca464519c7ae7d7c3eca6d6cdb772275b17e"
+  sha256 "2a2619a09b0af9a65d57137b2611c18aacfb9acca2486f79383ca4b80d2af723"
   license "PolyForm-Internal-Use-1.0.0"
 
   depends_on :macos
