e58bb597fa842bf82876639c7f83dd1bbfe6cb2a
Ziyang Tan <arcopus07@gmail.com>
2026-06-07 17:39:35 +0800

    Fix sha256 format in bBpiano-L.rb

--- Formula/bBpiano-L.rb
+++ Formula/bBpiano-L.rb
@@ -2,7 +2,7 @@ class BbpianoL < Formula
   desc "Command-line acoustic engine laboratory for bBpiano Lite"
   homepage "https://github.com/opus-arc/bBpiano"
   url "https://github.com/opus-arc/bBpiano/releases/download/v0.1.0-L/bBpiano-Lite-v0.1.0-L-macOS-arm64.zip"
-  sha256 "sha256:90c080c1fd705a5b0fac29829f5cca464519c7ae7d7c3eca6d6cdb772275b17e"
+  sha256 "90c080c1fd705a5b0fac29829f5cca464519c7ae7d7c3eca6d6cdb772275b17e"
   license "PolyForm-Internal-Use-1.0.0"
 
   depends_on :macos
