6e5a42145ef957b9b0b38e56421982911c145fce
Ziyang Tan <arcopus07@gmail.com>
2026-09-20 14:15:35 +0800

    Update cppobf formula to 0.1.1

--- Formula/cppobf.rb
+++ Formula/cppobf.rb
@@ -1,11 +1,11 @@
 class Cppobf < Formula
   desc "Modern C++ source obfuscation frontend for COBF"
   homepage "https://github.com/opus-arc/cppobf"
-  url "https://github.com/opus-arc/cppobf/releases/download/v0.1.0/cppobf-v0.1.0-macos-arm64.tar.gz"
-  sha256 "5665468ab9fa425661088d9adbbfdf88f7c55af833b7ce68fde5e6c8ccef7447"
+  url "https://github.com/opus-arc/cppobf/releases/download/v0.1.1/cppobf-v0.1.1-macos-arm64.tar.gz"
+  sha256 "867eac77bc238a868866d05eabce9e124b6b0dfaab5e0d2bd367be7d63e5c869"
 
-  depends_on :macos
   depends_on arch: :arm64
+  depends_on :macos
 
   def install
     libexec.install "cppobf", "presets", "vendor"
@@ -14,6 +14,6 @@ class Cppobf < Formula
   end
 
   test do
-    assert_match "cppobf 0.1.0", shell_output("#{bin}/cppobf --version")
+    assert_match "cppobf 0.1.1", shell_output("#{bin}/cppobf --version")
   end
 end
